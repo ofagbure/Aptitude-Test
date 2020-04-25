461867e042e98accce6cb09917d4dbbf5e72a3f7
TheMatthewSawyer <matthewleonardsawyer@gmail.com>
2020-04-25 01:16:57 -0400

    More syntax fixes to the sql schema.

--- db/schema.sql
+++ db/schema.sql
@@ -5,15 +5,15 @@ USE `aptitude_db`;
 
 
 CREATE TABLE user (
-id int ( 8 ) NOT NULL AUTO_INCREMENT,
+id int NOT NULL AUTO_INCREMENT,
 email varchar( 50 ) NOT NULL UNIQUE,
 usrpass varchar( 32 ) NOT NULL,
 employer boolean NOT NULL,
 PRIMARY KEY ( id )
-)
+);
 
 CREATE TABLE userprofile (
-profileid int ( 8 ) NOT NULL AUTO_INCREMENT,
+profileid int NOT NULL AUTO_INCREMENT,
 profileimg varchar ( 64 ),
 firstname varchar ( 64 ) NOT NULL,
 lastname varchar ( 64 ) NOT NULL,
@@ -24,18 +24,18 @@ descrip varchar( 512 ),
 city varchar ( 64 ) NOT NULL,
 willmove boolean NOT NULL,
 testresults varchar ( 64 ),
-id int ( 8 ) NOT NULL,
+id int NOT NULL,
 PRIMARY KEY ( profileid )
-)
+);
 
 CREATE TABLE employerprofile (
-companyid int ( 8 ) NOT NULL AUTO_INCREMENT,
+companyid int NOT NULL AUTO_INCREMENT,
 companyname varchar( 64 ) NOT NULL UNIQUE,
 companyimg varchar ( 64 ),
 tagline varchar( 64 ),
 website varchar ( 64 ),
 descrip varchar( 512 ),
 resultinterest varchar ( 64 ),
-id int ( 8 ) NOT NULL,
+id int NOT NULL,
 PRIMARY KEY ( companyid )
-)
+);
