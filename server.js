b41a8a4c45ec4be14ed7a9693e58cb3fe5b9c49d
TheMatthewSawyer <matthewleonardsawyer@gmail.com>
2020-04-25 13:43:06 -0400

    Basic server added with an api route.

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+const users = require("./config/orm.js");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-express.Router().get("/", (req, res) => {
-    burger.allUsers( function (result) {
+// This can be moved to a different controller file
+// Right now it's good for testing
+app.get("/api/allusers", (req, res) => {
+    users.allUsers( function (result) {
         let users = [];
         for(var i = 0; i < result.length; i++) {
-            users += result[i];
+            users += 
+                `<p>user_id: ${result[i].user_id}</p>
+                <p>email: ${result[i].email}</p>
+                <p>usr_pass: ${result[i].usr_pass}</p>
+                <p>recruiter? ${result[i].recruiter}</p>
+                --------------------------------------------
+                `;
         }
         res.send(users);
     });
